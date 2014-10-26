e62fd07d3d449273a312d600e2e69415b013d677
Mike Dillon <mike.dillon@synctree.com>
2014-10-25 21:29:50 -0700

    Add content negotiation
    * Supports HTML, JSON, and YAML

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'erubis'
+gem 'rack-conneg', git: 'https://github.com/md5/rack-conneg.git', branch: 'nil-fallback'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,20 +1,55 @@
 require 'sinatra'
+require 'rack/conneg'
 require 'erubis'
+require 'json'
+require 'yaml'
 
-set :erb, :escape_html => true
+set :erb, escape_html: true
+
+use Rack::Conneg do |c|
+  c.set :accept_all_extensions, false
+  c.set :fallback, nil
+  c.provide %i(html json yaml)
+end
 
 get '/*' do
-  erb :echo
+  echo
 end
 
 put '/*' do
-  erb :echo
+  echo
 end
 
 post '/*' do
-  erb :echo
+  echo
 end
 
 delete '/*' do
-  erb :echo
+  echo
+end
+
+def data
+  {
+    request: request.env.select { |k,_| k =~ /^(?!HTTP_)[A-Z]/ },
+    headers: request.env.select { |k,_| k =~ /^HTTP_/ },
+    params: request.params
+  }
+end
+
+def echo
+  respond_to do |wants|
+    wants.html do
+      erb :echo
+    end
+    wants.json do
+      JSON.pretty_generate data
+    end
+    wants.yaml do
+      YAML.dump data
+    end
+    wants.other do
+      content_type 'text/plain'
+      error 406, "Not Acceptable #{negotiated_type}\n"
+    end
+  end
 end
